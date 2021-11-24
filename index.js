a6d4cf6358732bbe1505b2c7a30f0ec88d2c5417
kevinyvr <kkwok39@my.bcit.ca>
2021-11-23 19:29:37 -0800

    milestone2 createcard

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("homepage");
+  res.render("createcard");
 });
 app.get("/people/:id", (req, res) => {
   res.render("people");
